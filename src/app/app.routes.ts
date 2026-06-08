832706de5bfbcf9b6879dece86a27c80e44809cb
reto <rspicher83@gmail.com>
2026-06-08 16:23:38 +0200

    deleted old dart-detector

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,26 +2,20 @@ import {Routes} from '@angular/router';
 import {Game} from './features/game/game';
 import {Calibration} from './features/calibration/calibration';
 import {Settings} from './features/settings/settings';
-import {DartDetector} from './dart-detector/dart-detector';
-import {CameraView} from './shared/camera-view/camera-view';
 import {Dashboard} from './dashboard/dashboard';
 
 export const routes: Routes = [
   {
     path: '',
-    component: Dashboard
-  },
-  {
-    path: 'dart',
-    component: DartDetector
-  },
-  {
-    path: 'tst',
-    component: CameraView
+    component: Game
   },
   {
     path: 'game',
     component: Game
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard
   },
   {
     path: 'calibration',
