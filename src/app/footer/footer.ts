f08cb4e9b00a1ce97c6e2ea1742605f3f00804db
reto <rspicher83@gmail.com>
2026-06-23 06:10:40 +0200

    publish version 1.0.9

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {
 
+  readonly currentYear =
+    new Date()
+      .getFullYear();
 }
